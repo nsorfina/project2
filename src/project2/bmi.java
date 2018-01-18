0c03c4c1712df85063e85a221225b4750ea10b4f
ICTKVPJB <ICTKVPJB@ICTKVPJB-PC>
2018-01-18 10:27:44 +0800

    latihan1

---
+++ src/project2/bmi.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Scanner;
+public class bmi 
+{
+    public static void main(String args[])
+{
+    Scanner input=new Scanner(System.in);
+    
+    System.out.print("masukkan berat anda:");
+    double berat = input.nextDouble();
+    
+    System.out.print("masukkan tinggi anda:");
+    double tinggi = input.nextDouble();
+    
+    double jum_bmi=(berat/tinggi * tinggi);
+    
+    if (jum_bmi <18.5)
+    {System.out.print("kurang berat badan");}
+    
+    else if (jum_bmi <25 && jum_bmi >185)
+    {System.out.print("normal");}
+    
+    else if (jum_bmi <30 && jum_bmi >25)
+    {System.out.print("berat badan berlebihan");}
+    
+    else
+    {System.out.print("gemuk");}
+    
+    {System.out.println("index bmi anda ialah "+jum_bmi);}
+}
+   
+    
+}
